e33649e2ae75f35e3530010e0c81c66ea9de2fc9
bucomar <59670765+bucomar@users.noreply.github.com>
2020-06-06 11:35:18 +0200

    Misi első két programja feltöltve.

---
+++ hello_MM.py
@@ -0,0 +1,13 @@
+#Misi elsö python programja
+
+MM = 'Misi'
+MA = 'Bandi'
+
+print(MM + ' es ' + MA)
+
+nev = input('szevasz, hogy hivnak?: ')
+print('Hello ' + nev)
+
+szin = input(nev + ', mi a kedvenc szined? :')
+allat = input(nev + ', mi a kedvenc allatod? :')
+print('Hello ' + nev + ', kapsz tolem ajandekba egy ' + szin + allat + '-t. °°-\___')
---
+++ szerelmes_szamok.py
@@ -0,0 +1,14 @@
+# Szerelmes szamok - Misi 2. programja
+
+print('A szerelmes szamok a kovetkezok:')
+print('0 + 10')
+print('1 + 9')
+print('2 + 8')
+print('3 + 7')
+print('4 + 6')
+print('5 + 5')
+print()
+
+szam = input('Kerek egy egesz szamot 0 es 10 kozott!: ')
+print('Ennek a szamnak a szerelme a/az : ' + str(10 - int(szam)))
+print('Te is igy gondoltad? ;-)')
